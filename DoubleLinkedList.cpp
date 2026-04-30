595321755145817b334ced339f94436c46f6f041
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-04-30 12:56:17 +0700

    menambahkan fungsi hapus dengan pengecekan list kosong dan input data

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -67,7 +67,22 @@ public:
 
         if (current->next != NULL)
             current->next->prev = newNode;
-            
+
         current->next = newNode;
+    }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
     }
 };
