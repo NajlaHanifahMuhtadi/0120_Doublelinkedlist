a7447e18a10c001b2abfd6da7ae5cef4c79fd5eb
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-04-30 13:31:24 +0700

    menghubungkan menu dengan fungsi menggunakan switchcase

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -205,5 +205,33 @@ int main()
         cout << "Enter your choice: ";
 
         cin >> choice;
-    }
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.revtraverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "Invalid option\n" << endl;
+        }
+        cout << "\nPress enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+    } while (choice != '6');
 };
